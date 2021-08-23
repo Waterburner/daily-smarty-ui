5a2e523ab08f754e19e9a64a70de709a94da7830
Waterburner <Waterburner@users.noreply.github.com>
2021-08-23 15:37:19 -0500

    corrected import

--- src/components/results.js
+++ src/components/results.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-import Logo from "../../static/assets/logo/ds_circle_logo1.png";
+import Logo from "./logo";
 
 export default class Results extends Component {
     render() {
-        return <div></div>;
+        return (
+            <div>
+                <h1>Results</h1>
+                <Logo />
+            </div>
+        );
     }
 }
